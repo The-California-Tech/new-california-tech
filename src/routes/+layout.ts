770c149ddf9e06a7fce26def1a0d5218e331ea1f
Michael Gutierrez <findmagpie@gmail.com>
2026-04-04 12:48:39 -0700

    add vercel insights

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,13 @@
 export const prerender = true;
 import type { LayoutLoad } from './$types';
 
+import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+injectSpeedInsights();
+injectAnalytics({ mode: dev ? 'development' : 'production' });
+
 export const load: LayoutLoad = async ({ url }) => {
   return {
     props: {
